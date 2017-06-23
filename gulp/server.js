845e68e4a52af6d6cd08697779f2081351e2c8db
sinnkami <sinnkami.suraidaku@gmail.com>
2017-06-23 12:36:45 +0900

    var -> constに変更した

--- gulp/server.js
+++ gulp/server.js
@@ -1,7 +1,7 @@
 const gls = require('gulp-live-server');
 const gulp = require('gulp');
 const value = require('./lib/value');
-var server = gls(`${value.dest}/bin/www`, undefined, false);
+const server = gls(`${value.dest}/bin/www`, undefined, false);
 
 gulp.task('server', function () {
   if (server) { server.stop(); }
