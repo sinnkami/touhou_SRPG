50608d017c1ce1fb0318cb4473d4bc581269bf78
sinnkami <sinnkami.suraidaku@gmail.com>
2017-06-24 14:35:17 +0900

    viewsもelectron対応に変更した

--- gulp/views.js
+++ gulp/views.js
@@ -2,6 +2,6 @@ const gulp = require('gulp');
 const args = require('./lib/value');
 
 gulp.task('views', function () {
-  return gulp.src(`${args.src}/views/**/*.jade`)
+  return gulp.src(`${args.src}/views/${args.vendor}/**/*.jade`)
   .pipe(gulp.dest(`${args.dest}/views`))
 })
