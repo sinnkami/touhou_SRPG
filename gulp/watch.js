57803245faa7c06fbdd3681042efdeeb9a279555
sinnkami <sinnkami.suraidaku@gmail.com>
2017-07-09 18:37:57 +0900

    watchをpublic全体に変更した

--- gulp/watch.js
+++ gulp/watch.js
@@ -6,10 +6,7 @@ gulp.task('watch:express', (callback) => {
   if (!args.watch) { return callback(); }
 
   watch(`${args.src}/bin/www`, () => { return gulp.start(['bin', 'server']); });
-  watch(`${args.src}/public/js/**/*.js`, () => { return gulp.start(['scripts']); });
-  watch(`${args.src}/public/scss/**/*.scss`, () => { return gulp.start(['style:sass']); });
-  watch(`${args.src}/public/css/**/*.css`, () => { return gulp.start(['style:css']); });
-  watch(`${args.src}/public/images/**/*.`, () => { return gulp.start(['images']); });
+  watch(`${args.src}/public/**/*`, () => { return gulp.start(['public']); });
   watch(`${args.src}/routes/**/*.js`, () => { return gulp.start(['routes', 'server']); });
   watch(`${args.src}/views/${args.vendor}/**/*.jade`, () => { return gulp.start(['views', 'server']); });
   watch(`${args.src}/${args.vendor}.js`, () => { return gulp.start([`${args.vendor}`, 'server']); });
@@ -18,10 +15,7 @@ gulp.task('watch:express', (callback) => {
 gulp.task('watch:electron', (callback) => {
   if (!args.watch) { return callback(); }
 
-  watch(`${args.src}/public/js/**/*.js`, () => { return gulp.start(['scripts']); });
-  watch(`${args.src}/public/scss/**/*.scss`, () => { return gulp.start(['style:sass']); });
-  watch(`${args.src}/public/css/**/*.css`, () => { return gulp.start(['style:css']); });
-  watch(`${args.src}/public/images/**/*.`, () => { return gulp.start(['images']); });
+  watch(`${args.src}/public/**/*`, () => { return gulp.start(['public']); });
   watch(`${args.src}/views/${args.vendor}/**/*.jade`, () => { return gulp.start(['views']); });
   watch(`${args.src}/${args.vendor}.js`, () => { return gulp.start([`${args.vendor}`]); });
 })
