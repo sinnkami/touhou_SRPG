1fc7aa0e2f2ddb3038540d03aa5c22dd6b211207
sinnkami <sinnkami.suraidaku@gmail.com>
2017-07-09 17:57:24 +0900

    作成したファイルを読み込んだ

--- app/public/js/Game/main.js
+++ app/public/js/Game/main.js
@@ -1,3 +1,14 @@
 const Manager = new Game_Manager([
-  new Game_Base([new Game_Player()])
+  new Data_Base([new Data_Player(), new Data_Map()]),
+  new Game_Base([new Game_Player()]),
+  new Scene_Base([]),
+  new Sprite_Base([new Sprite_Map()]),
+  new Window_Base([])
 ]);
+
+$(function () {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  Manager.canvas = canvas;
+  Manager.context = context;
+})
